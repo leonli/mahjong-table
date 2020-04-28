87768e210e3139ee765686916050f4c8e35e025e
Leon Li <lilei.leon@gmail.com>
2020-04-28 13:31:03 +0800

    first init mahjong table

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.scss";
-import Counter from "./features/counter/Counter";
 import { Provider } from "react-redux";
 import store from "./store";
+import NavMahjong from "features/nav/NavMahjong";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Counter />
-        </header>
+      <div className="App bp3-dark">
+        <NavMahjong></NavMahjong>
       </div>
     </Provider>
   );
---
+++ src/features/nav/NavMahjong.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "./logo.svg";
+import protoLogo from "./AWS-Prototyping_Logo_White.svg";
+//import { useDispatch, useSelector } from "react-redux";
+import { Button, Navbar, Alignment, Icon } from "@blueprintjs/core";
+import "normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+import styles from "./NavMahjong.module.scss";
+
+const NavMahjong: React.FC = () => {
+  return (
+    <Navbar>
+      <Navbar.Group align={Alignment.LEFT}>
+        <img src={logo} alt="logo" className={styles.applogo} />
+        <Navbar.Heading>Mahjong</Navbar.Heading>
+        <Navbar.Divider />
+        <span className="bp3-tag .modifier">v0.5.0</span>
+      </Navbar.Group>
+      <Navbar.Group align={Alignment.RIGHT}>
+        <div className="bp3-input-group .modifier">
+          <span className="bp3-icon bp3-icon-search"></span>
+          <input
+            className="bp3-input"
+            type="search"
+            placeholder="Search Docs"
+            dir="auto"
+          />
+        </div>
+        <Navbar.Divider />
+        <Button className="bp3-minimal" icon="document" text="Doc" />
+        <Button className="bp3-minimal" icon="git-repo" text="Github" />
+        <Navbar.Divider />
+        <span className={styles.madeBy}>Made with &nbsp;</span>
+        <Icon icon="heart"></Icon>
+        <span className={styles.madeBy}> &nbsp; by &nbsp;</span>
+        <Icon
+          icon={<img src={protoLogo} alt="logo" className={styles.protoLogo} />}
+        />
+      </Navbar.Group>
+    </Navbar>
+  );
+};
+
+export default NavMahjong;
